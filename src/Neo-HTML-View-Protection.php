339cc3cb48c62494b9dc844ffe4af750fe503fa4
nanoko <yy845jp@gmail.com>
2025-03-18 19:12:10 +0900

    v0.22

--- src/Neo-HTML-View-Protection.php
+++ src/Neo-HTML-View-Protection.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * Plugin Name: Neo HTML View Protection
- * Description: view-source: でもHTMLをわずか数文字にする
- * Version: 0.21
+ * Description: view-source: でもHTMLを見れないようにする
+ * Version: 0.23
  * Author: Nano Yozakura
  * License: GPL2
  */
 
-// 10秒のクッキーを発行してエンコードされたURLを保存
-function pgv_set_cookie_and_redirect() {
+// 9秒のクッキーを発行してエンコードされたURLを保存
+function nhvp_set_cookie_and_redirect() {
     // 現在のURLを取得
     $current_url = $_SERVER['REQUEST_URI'];
 
@@ -27,9 +27,19 @@ function pgv_set_cookie_and_redirect() {
     $request_uri = $_SERVER['REQUEST_URI'];
     $current_url = $scheme . '://' . $host . $request_uri;
 
+    // UNIXtime / 60
+    $min = floor(time() / 60);
+
+    // クッキー名を生成
+    $cookie_name = 'ne' . $min;
+
     // JavaScriptでエンコードされたURLをデコードしてリダイレクトするスクリプトを挿入
     $html = '<!doctype html><html lang="' . $lang . '"><head><meta charset="UTF-8">';
-    $html .= '<script>var neUrl="' . $neo_encoded_url . '";document.cookie="neo_encoded_url="+neUrl+";max-age=10;path=/";window.location.href=atob(neUrl);</script>';
+    $html .= '<script>';
+    $html .= 'var neUrl="' . $neo_encoded_url . '";';
+    $html .= 'document.cookie="' . $cookie_name . '="+neUrl+";max-age=9;path=/";';
+    $html .= 'window.location.href=atob(neUrl);';
+    $html .= '</script>';
     $html .= '<title>' . $title . '</title>';
 
     $meta_description = get_post_meta(get_the_ID(), 'meta_description', true);
@@ -51,9 +61,9 @@ function pgv_set_cookie_and_redirect() {
     $html .= '<meta property="og:url" content="' . $current_url . '">';
     $html .= '<meta property="og:site_name" content="' . get_bloginfo('name') . '">';
 
-    $feedbase=$scheme . '://' . $host;
-$html .= '<link rel="alternate" type="application/rss+xml" title="' . get_bloginfo('name') . ' &raquo; feed" href="' . $feedbase . '/feed/">';
-$html .= '<link rel="alternate" type="application/rss+xml" title="' . get_bloginfo('name') . ' &raquo; comment feed" href="' . $feedbase . '/comments/feed/">';	
+    $feedbase = $scheme . '://' . $host;
+    $html .= '<link rel="alternate" type="application/rss+xml" title="' . get_bloginfo('name') . ' &raquo; feed" href="' . $feedbase . '/feed/">';
+    $html .= '<link rel="alternate" type="application/rss+xml" title="' . get_bloginfo('name') . ' &raquo; comment feed" href="' . $feedbase . '/comments/feed/">';
 
     $html .= '</head></html>';
 
@@ -62,13 +72,37 @@ $html .= '<link rel="alternate" type="application/rss+xml" title="' . get_blogin
 }
 
 // クッキーからエンコードされたURLを取得し、リダイレクトする
-function pgv_redirect_from_cookie() {
-    if (isset($_COOKIE['neo_encoded_url'])) {
+function nhvp_redirect_from_cookie() {
+    // UNIXtime / 60
+    $min = floor(time() / 60);
+    $cookie_name = 'ne' . $min;
+
+    $min2 = floor(time() / 60);
+    $cookie_name2 = 'ne' . $min2;
+
+    if (isset($_COOKIE[$cookie_name])) {
         // クッキーにエンコードされたURLがある場合、デコードしてリダイレクト
-        $neo_encoded_url = $_COOKIE['neo_encoded_url'];
+        $neo_encoded_url = $_COOKIE[$cookie_name];
         $decoded_url = base64_decode($neo_encoded_url);
 
-        setcookie("neo_encoded_url", "", time() - 3600, "/"); 
+        // クッキーを削除
+        setcookie($cookie_name, "", time() - 3600, "/");
+
+        // 現在のページとリダイレクト先が異なる場合にリダイレクト
+        if ($_SERVER['REQUEST_URI'] !== $decoded_url) {
+            header("Location: $decoded_url");
+            exit;
+        }
+    }
+
+    // 1分前も処理する
+    if (isset($_COOKIE[$cookie_name2])) {
+        // クッキーにエンコードされたURLがある場合、デコードしてリダイレクト
+        $neo_encoded_url = $_COOKIE[$cookie_name2];
+        $decoded_url = base64_decode($neo_encoded_url);
+
+        // クッキーを削除
+        setcookie($cookie_name2, "", time() - 3600, "/");
 
         // 現在のページとリダイレクト先が異なる場合にリダイレクト
         if ($_SERVER['REQUEST_URI'] !== $decoded_url) {
@@ -79,22 +113,27 @@ function pgv_redirect_from_cookie() {
 }
 
 // クッキーがあるか確認してリダイレクトする
-function pgv_check_and_redirect() {
-    if(!is_user_logged_in()) {
+function nhvp_check_and_redirect() {
+    if (!is_user_logged_in()) {
         // RSSでないこと、POSTでないこと、コメントの時でないこと
-        if(strpos($_SERVER['REQUEST_URI'], '/feed/') === false
-        || $_SERVER['REQUEST_METHOD'] !== 'POST'
-        || !empty($_GET['unapproved']) || !empty($_GET['moderation-hash'])) {
+        if (strpos($_SERVER['REQUEST_URI'], '/feed/') === false
+            || $_SERVER['REQUEST_METHOD'] !== 'POST'
+            || !empty($_GET['unapproved']) || !empty($_GET['moderation-hash'])) {
+
+            // UNIXtime / 60
+            $min = floor(time() / 60);
+            $cookie_name = 'ne' . $min;
+
             // クッキーがセットされている場合、リダイレクト処理を実行
-            if (isset($_COOKIE['neo_encoded_url'])) {
-                pgv_redirect_from_cookie();
+            if (isset($_COOKIE[$cookie_name])) {
+                nhvp_redirect_from_cookie();
             } else {
-                // クッキーがない場合、10秒のクッキーを発行してリダイレクト
-                pgv_set_cookie_and_redirect();
+                // クッキーがない場合、9秒のクッキーを発行してリダイレクト
+                nhvp_set_cookie_and_redirect();
             }
         }
     }
 }
 
 // 高い優先度でリダイレクト処理を追加（template_redirectフックを使用）
-add_action('template_redirect', 'pgv_check_and_redirect', 1);
+add_action('template_redirect', 'nhvp_check_and_redirect', 1);
